eeebc18b04130bc150bda3c48bb12dff706fb7c8
wilbefast <wilbefast@gmail.com>
2013-05-10 23:56:03 +0200

    player creates clones when moving

--- src/gameobjects/Player.lua
+++ src/gameobjects/Player.lua
@@ -25,11 +25,15 @@ local Player = Class
   type = GameObject.TYPE.new("Player"),
       
   speed = 64,
+  
+  next_universe = 1,
 
   init = function(self, x, y)
     GameObject.init(self, x, y, 30, 30)
     self.targetX = x
     self.targetY = y
+    self.universe = Player.next_universe
+    Player.next_universe = Player.next_universe + 1
   end,
 }
 Player:include(GameObject)
@@ -42,12 +46,17 @@ Game loop
 function Player:update(dt)
   -- default update
   GameObject.update(self, dt)
-  
-  
-  
+
+  -- cache
   local grid = GameObject.COLLISIONGRID
   
-
+  -- only the original universe player is controlled
+  local dx, dy = 0, 0
+  if self.universe == 1 then
+    dx, dy = input.x, input.y
+  else
+  end
+  
   -- HORIZONTAL MOVEMENT
   ---------------------------------------------------------
   -- local variables
@@ -55,17 +64,21 @@ function Player:update(dt)
     = ((self.targetX - self.x)*self.dx < 0)
   local collisionX 
     = grid:collision(self, 
-        self.x + input.x*grid.tilew/2, self.y)
+        self.x + dx*grid.tilew/2, self.y)
   -- starting moving
-  if (math.abs(input.x) > 0) and (not collisionX) 
+  if (math.abs(dx) > 0) and (not collisionX) 
   and (self.dy == 0) then
     if overShotX then
-      local f = useful.tri(input.x > 0, 
+      -- create alternate version
+      Player(self.targetX, self.targetY)
+      -- reset target
+      local f = useful.tri(dx > 0, 
                 useful.floor, useful.ceil)
       self.targetX = f(self.x, grid.tilew) 
-                    + grid.tilew*input.x
+                    + grid.tilew*dx
+
     end
-    self.dx = input.x * self.speed
+    self.dx = dx * self.speed
   -- stop at destination
   elseif overShotX then
     self.x = self.targetX
@@ -79,17 +92,20 @@ function Player:update(dt)
     = ((self.targetY - self.y)*self.dy < 0)
   local collisionY 
     = grid:collision(self, 
-        self.x, self.y + input.y*grid.tileh/2)
+        self.x, self.y + dy*grid.tileh/2)
   -- starting moving
-  if (math.abs(input.y) > 0) and (not collisionY)
+  if (math.abs(dy) > 0) and (not collisionY)
   and (self.dx == 0) then
     if overShotY then
-      local f = useful.tri(input.y > 0, 
+      -- create alternate version
+      Player(self.targetX, self.targetY)
+      -- reset target
+      local f = useful.tri(dy > 0, 
                 useful.floor, useful.ceil)
       self.targetY = f(self.y, grid.tilew) 
-                    + grid.tileh*input.y
+                    + grid.tileh*dy
     end
-    self.dy = input.y * self.speed
+    self.dy = dy * self.speed
   -- stop at destination
   elseif overShotY then
     self.y = self.targetY
@@ -104,10 +120,6 @@ function Player:draw()
   
   local grid = GameObject.COLLISIONGRID
   local bink = grid:collision(self)
-  
-  love.graphics.setColor(255, 0, 0)
-  love.graphics.rectangle(useful.tri(bink, "fill", "line"), self.targetX, self.targetY, self.w, self.h)
-  love.graphics.setColor(255, 255, 255)
 end
 
 
